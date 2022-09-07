83337dd6ab2bdfa55b529f7e61fc0d725cb14163
zrogba <zrogba@gmail.com>
2022-09-07 12:09:44 +0300

    add forecast componet and stylings

--- src/App.js
+++ src/App.js
@@ -3,35 +3,36 @@ import Search from './components/search/search';
 import CurrentWeather from './components/search/current-weather/current-weather';
 import { WEATHER_API_URL, WEATHER_API_KEY } from './Api';
 import { useState } from 'react';
+import Forecast from './components/forecast/forecast';
 
 function App() {
-//call function const handleOnSearchChange = (searchData) =>{
+  //call function const handleOnSearchChange = (searchData) =>{
   //create hook functions to store 
   const [currenWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
 
-  const handleOnSearchChange = (searchData) =>{
+  const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
 
-    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}` );
-    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}` );
-  
-//use promise to  pass the arrays of weather and forecast,
-//and call async function and call th ejson method to  map the 
-//create hook functions to store 
-//extend the data to complete with label of full description 
-//by using geoapi data add city parameter with searchData and label
-Promise.all([currentWeatherFetch, forecastFetch])
-.then(async (response) => {
-  const weatherResponse = await response[0].json();
-  const forecastResponse = await response[1].json();
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
 
-  setCurrentWeather({ city: searchData.label, ...weatherResponse});
-  setForecast({ city: searchData.label, ...forecastResponse});
+    //use promise to  pass the arrays of weather and forecast,
+    //and call async function and call th ejson method to  map the 
+    //create hook functions to store 
+    //extend the data to complete with label of full description 
+    //by using geoapi data add city parameter with searchData and label
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
 
-})
-//use of .catch if it fails  and console 
-  .catch((err) => console.log(err));
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+
+      })
+      //use of .catch if it fails  and console 
+      .catch((err) => console.log(err));
   }
 
   console.log(currenWeather);
@@ -39,13 +40,14 @@ Promise.all([currentWeatherFetch, forecastFetch])
 
   return (
     <div className="container">
-     
+
       <Search onSearchChange={handleOnSearchChange} />
-     {currenWeather && <CurrentWeather data={currenWeather} />}
+      {currenWeather && <CurrentWeather data={currenWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
 
 export default App;
-// create two fects make two api calls one for forecast and second for 
+// create two fects make two api calls one for forecast and second for
 //get api kets and url for forcast and current weather frm open api
--- src/components/search/current-weather/current-weather.js
+++ src/components/search/current-weather/current-weather.js
@@ -1,30 +1,34 @@
 import "./current-weather.css"
 
-const CurrentWeather = ({data}) => {
+const CurrentWeather = ({ data }) => {
     return (
         <div className="weather">
             <div className="top">
                 <div>
-                <p className="city">{data.city}</p>
-                <p className="weather-description">{data.weather[0].description}</p>
-            </div>
-            <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`}/>
+                    <p className="city">{data.city}</p>
+                    <p className="weather-description">{data.weather[0].description}</p>
+                </div>
+                <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
             </div>
 
             <div className="bottom">
-                <p className="temprature">18°C</p>
+                <p className="temprature">{Math.round(data.main.temp)} °C</p>
                 <div className="details">
                     <div className="parameter-row">
                         <span className="parameter-label">Feels like </span>
-                        <span className="parameter-value">22°C</span>
+                        <span className="parameter-value">{Math.round(data.main.feels_like)} °C</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Wind</span>
-                        <span className="parameter-value">2 m/s</span>
+                        <span className="parameter-value">{data.wind.speed} m/s</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity</span>
+                        <span className="parameter-value">{data.main.humidity} %</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Pressure</span>
-                        <span className="parameter-value">14 hpa</span>
+                        <span className="parameter-value">{data.main.pressure} hpa</span>
                     </div>
                 </div>
             </div>
